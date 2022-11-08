94b7fa3063a729fdf22e930b9598df52f382ecc0
hamza-ilyas1 <hilyas2131@gmail.com>
2022-11-08 11:37:48 -0500

    Github import

---
+++ FunLeague/README.md
@@ -0,0 +1,20 @@
+# FunLeague
+
+## What is FunLeague?
+
+• Funleague is free gaming website. There are over 500 free games available on FunLeague. User can also post their own games on our website.
+
+Check it out here: https://funleague.netlify.app/
+
+## Technologies
+
+here are the technologies utilized for FunLeague:
+
+• Frontend: JavaScript, React, CSS - material ui
+• Hosted frontend on Netlify and api database on heroku.
+
+## Other Dependencies
+
+In the project directory, you can run:
+
+npm install
---
+++ FunLeague/src/Components/GameCard.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+function GameCard({game}) {
+
+  const [liked, setLiked] = useState(false)
+
+  function handleLike () {
+    setLiked(true)
+  }
+
+  function handleDislike () {
+    setLiked(false)
+  }
+  
+    return (
+       <Card id='card-container' sx={{ maxWidth: 345 }}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={game.title}
+        subheader={game.genre}
+      />
+      {<a href={game.game_url} target='_blank' rel='noreferrer'>
+      <CardMedia
+        component="img"
+        height="194"
+        image={game.thumbnail}
+        alt="Paella dish"
+      /> </a>}
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+         {game.short_description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+         {liked ? <FavoriteIcon onClick={handleDislike} id='like'/>  : <FavoriteIcon onClick={handleLike}/>}
+        </IconButton>
+      </CardActions>
+    </Card>
+    )
+}
+
+export default GameCard
